2d81888bcba1630edcfd7d167fca93df0d21d9a5
Yaroslav-develop <jar.shap@yandex.ru>
2026-04-09 22:47:13 +0300

    Add files via upload

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    public Transform groundCheck;
+    public float checkRadius = 0.3f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private float moveInput;
+    private bool isGrounded;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        if (Input.GetKeyDown(KeyCode.R) && isGrounded)
+        {
+            rb.velocity = new Vector2(jumpForce * 10, rb.velocity.y);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck == null) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, checkRadius);
+    }
+}
