048b54857385c72ca14498b0cafdf9ef6d327bed
Илья Эфендиев <dreamsayori@gmail.com>
2026-05-13 23:38:48 +0300

    Интеграция с ИИ

---
+++ EnemyDamage.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public int damage = 1;
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerHealth player = collision.gameObject.GetComponent<PlayerHealth>();
+
+            if (player != null)
+            {
+                player.TakeDamage(damage);
+            }
+
+            // helllo world
+            //копайлотом тут не пахнет. Здесь замешан DeepSeek
+        }
+    }
+}
